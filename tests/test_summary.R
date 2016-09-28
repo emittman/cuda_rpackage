5008278c25d974f2ea40cb30fe65a2427ddd87ff
emittman <emittman@iastate.edu>
2016-09-28 14:50:29 -0500

    Try to print output

--- tests/test_summary.R
+++ tests/test_summary.R
@@ -1,7 +1,9 @@
 #This should get replaced by a testthat function
 
 x <- matrix(sample(1:8, 12, replace=T), 3, 4)
+key <- c(0, 1, 1, 2)
+num_clust <- 4
+output <- Rsummary(x, key, num_clust)
 
-Rsummary(x, c(0, 1, 1, 2), 4)
-
+(output)
 #first elt should be "[1, 2, 1, 0]"
