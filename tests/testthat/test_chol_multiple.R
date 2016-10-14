c0fc8c955234346bcc3079f37a463543aff20520
emittman <emittman@iastate.edu>
2016-10-14 14:38:47 -0500

    Add comments

--- tests/testthat/test_chol_multiple.R
+++ tests/testthat/test_chol_multiple.R
@@ -3,17 +3,20 @@ context("Testing cholesky decomposition")
 dim <- 5
 n <- 100
 
+#create random symmetric positive definite matrix
 make_spd <- function(dim){
   x <- matrix(round(rnorm(dim^2)*3, 2), dim, dim)
   return(x %*% t(x))
 }
 
+# Create an array of spd matrices
 make_spd_array <- function(dim, n){
   y <- sapply(1:n, function(x) as.numeric(make_spd(dim)))
   dim(y) <- c(dim, dim, n)
   return(y)
 }
 
+# Find lower cholesky facotorization of all spd matrices in array
 Rchol <- function(spd_array){
   d <- dim(spd_array)
   y <- apply(spd_array, 3, function(x) t(chol(matrix(x, d[1], d[2]))))
@@ -21,11 +24,14 @@ Rchol <- function(spd_array){
   return(y)
 }
 
+# Are lower triangular parts of two arrays are equal?
 compare_chol <- function(sub_array1, sub_array2, dim){
   tri <- lower.tri(sub_array1)
   return(all.equal(tri*sub_array1, tri*sub_array2))
 }
 
+
+# Compare (lower triangular part of) all 2 dim submatrices in array of matrices
 do_compare <- function(spd_array, n){
   chol1 <- Rchol(spd_array)
   chol2 <- Rchol_multi(spd_array)
