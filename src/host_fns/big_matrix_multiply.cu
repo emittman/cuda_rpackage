686a1a5be8bd29f3a7ed8580863600e99a6b8fd6
emittman <emittman@iastate.edu>
2017-01-13 10:53:48 -0600

    OK

--- src/host_fns/big_matrix_multiply.cu
+++ src/host_fns/big_matrix_multiply.cu
@@ -13,8 +13,8 @@ void big_matrix_multiply(fvec_d &A, fvec_d &B, fvec_d &big_grid, int a1, int a2,
   double *B_ptr = thrust::raw_pointer_cast(&(B[0]));
   double *grid_ptr = thrust::raw_pointer_cast(&(big_grid[0]));
   cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, a2, b2, a1,
-              &alpha, beta_ptr, lda,
-              xty_ptr, ldb,
+              &alpha, A_ptr, lda,
+              B_ptr, ldb,
               &beta, grid_ptr, ldc);
   cublasDestroy(handle);
 }
