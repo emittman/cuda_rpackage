f9675d4a82993a889f81b2fb64a35c82bdcc7db5
emittman <emittman@iastate.edu>
2017-01-13 14:05:09 -0600

    More corrections

--- src/tests.cu
+++ src/tests.cu
@@ -6,16 +6,15 @@
 #include <Rmath.h>
 
 
-extern "C" SEXP Rdata_init(SEXP ytyR, SEXP xtyR, SEXP xtxR, SEXP G, SEXP V, SEXP N){
-  int g = INTEGER(G)[0], v = INTEGER(V)[0], k = INTEGER(K)[0];
-  int *zp = INTEGER(zeta);
+extern "C" void Rdata_init(SEXP ytyR, SEXP xtyR, SEXP xtxR, SEXP G, SEXP V, SEXP N){
+  int g = INTEGER(G)[0], v = INTEGER(V)[0], n = INTEGER(N)[0];
   fvec_h xtx(v*v, 1.0);
   double *ytyp = REAL(ytyR);
   double *xtyp = REAL(xtyR);
   double *xtxp = &(xtx[0]);
-  data_t data(ytyp, xtyp, xtxp, g, v, 1);
-  printVec(data.xtx, V, V);
-  printVec(data.xty, V, G);
-  printVec(data.ytx, G, V);
+  data_t data(ytyp, xtyp, xtxp, g, v, n);
+  printVec(data.xtx, v, v);
+  printVec(data.xty, v, g);
+  printVec(data.ytx, g, v);
 }
 
