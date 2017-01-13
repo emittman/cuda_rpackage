65c85bd57ef1be96c61382cd8482491034939824
emittman <emittman@iastate.edu>
2017-01-13 14:18:41 -0600

    Think the problem was void return type...

--- src/tests.cu
+++ src/tests.cu
@@ -6,15 +6,18 @@
 #include <Rmath.h>
 
 
-extern "C" void Rdata_init(SEXP ytyR, SEXP xtyR, SEXP xtxR, SEXP G, SEXP V, SEXP N){
+extern "C" SEXP Rdata_init(SEXP ytyR, SEXP xtyR, SEXP xtxR, SEXP G, SEXP V, SEXP N){
   int g = INTEGER(G)[0], v = INTEGER(V)[0], n = INTEGER(N)[0];
   fvec_h xtx(v*v, 1.0);
   double *ytyp = REAL(ytyR);
   double *xtyp = REAL(xtyR);
   double *xtxp = &(xtx[0]);
   data_t data(ytyp, xtyp, xtxp, g, v, n);
-  //printVec(data.xtx, v, v);
-  //printVec(data.xty, v, g);
-  //printVec(data.ytx, g, v);
+  printVec(data.xtx, v, v);
+  printVec(data.xty, v, g);
+  printVec(data.ytx, g, v);
+  SEXP zero = PROTECT(allocVector(INTSXP, 1));
+  INTEGER(zero)[0] = 0;
+  return zero;
 }
 
