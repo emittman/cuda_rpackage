c08095d4365769b8602668915b42ad6ee4e4edd7
emittman <emittman@iastate.edu>
2017-01-13 16:28:51 -0600

    Switch from 0-indexing for R

--- tests/testthat/test_cluster_weights.R
+++ tests/testthat/test_cluster_weights.R
@@ -1,7 +1,7 @@
 context("Calculating cluster weights")
 
-G <- as.integer(1000)
-K <- as.integer(1000)
+G <- as.integer(10)
+K <- as.integer(5)
 V <- as.integer(2)
 N <- as.integer(3)
 
@@ -16,11 +16,13 @@ bxty <- rnorm(G*K)
 
 bxxb <- rnorm(K)
 
-Rout <- sapply(1:G*K, function(i){
-  idk <- i %% K
-  idg <- floor(i / K)
-  log(pi[idk]) + 0.5 * V*N * log(tau2[idk]) + .05 * tau2[idk] * (yty[idg] - 2*bxty[i] + bxxb[idk])
-})
+Rout <- unlist(sapply(1:(G*K), function(i){
+  idk <- (i %% K)+1
+  idg <- (floor(i / G))+1
+  sprintf("%d, %d\n", idk, idg)
+  out <- log(pi[idk]) + 0.5 * V*N * log(tau2[idk]) + .05 * tau2[idk] * (yty[idg] - 2*bxty[i] + bxxb[idk])
+  return(out)
+}))
 
 Cout <- .Call("Rcluster_weights", bxty, pi, tau2, yty, bxxb, G, V, N, K)
 
