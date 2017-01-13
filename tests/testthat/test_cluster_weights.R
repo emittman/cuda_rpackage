1ccb43f2aee35d91c8deebaf0d7f448ab79cc142
emittman <emittman@iastate.edu>
2017-01-13 16:51:45 -0600

    And...

--- tests/testthat/test_cluster_weights.R
+++ tests/testthat/test_cluster_weights.R
@@ -18,7 +18,7 @@ bxxb <- rnorm(K)
 
 idk <- rep(1:K, times=G)
 idg <- rep(1:G, each=K)
-Rout <- log(pi[idk]) + V*N* tau2[idk] + tau2[idk] * (yty[idg] - 2*bxty[1:(G*K)] + bxxb[idk])
+Rout <- log(pi[idk]) + 0.5 * V*N* log(tau2[idk]) - 0.5 * tau2[idk] * (yty[idg] - 2*bxty[1:(G*K)] + bxxb[idk])
 
 
 Cout <- .Call("Rcluster_weights", bxty, pi, tau2, yty, bxxb, G, V, N, K)
