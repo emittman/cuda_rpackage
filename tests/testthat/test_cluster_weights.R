b0ba9b849cf766e273c94b2da030a1ba71e71f6d
emittman <emittman@iastate.edu>
2017-01-13 21:05:24 -0600

    More thourough test

--- tests/testthat/test_cluster_weights.R
+++ tests/testthat/test_cluster_weights.R
@@ -1,28 +1,32 @@
 context("Calculating cluster weights")
 
-G <- as.integer(10)
-K <- as.integer(5)
+G <- as.integer(1000)
+K <- as.integer(50)
 V <- as.integer(2)
-N <- as.integer(3)
+N <- as.integer(4)
 
-pi <- rep(1, K)
-# pi <- pi/sum(pi)
+times <- 10
 
-tau2 <- rep(1, K)#/rexp(K)
-
-yty <- rep(0, G)
-
-bxty <- rep(1, G*K)#rnorm(G*K)
-
-bxxb <- rep(1.1, K) #rnorm(K)
-
-idk <- rep(1:K, times=G)
-idg <- rep(1:G, each=K)
-Rout <- log(pi[idk]) + 0.5 * V * N * log(tau2[idk]) + -0.5 * tau2[idk] * (yty[idg] - 2*bxty[1:(G*K)] + bxxb[idk])
-
-
-Cout <- .Call("Rcluster_weights", bxty, pi, tau2, yty, bxxb, G, V, N, K)
-
-test_that("Weights are correct", {
-  expect_equal(Rout, Cout)
+plyr::ldply(1:times, function(i){
+  pi <- rbeta(K, 1, 1)
+  pi <- pi/sum(pi)
+  
+  tau2 <- 1/rexp(K, 1)
+  
+  yty <- (rnorm(G))^2
+  
+  bxty <- rnorm(G*K)
+  
+  bxxb <- (rnorm(K))^2
+  
+  idk <- rep(1:K, times=G)
+  idg <- rep(1:G, each=K)
+  Rout <- log(pi[idk]) + 0.5 * V * N * log(tau2[idk]) + -0.5 * tau2[idk] * (yty[idg] - 2*bxty[1:(G*K)] + bxxb[idk])
+  
+  
+  Cout <- .Call("Rcluster_weights", bxty, pi, tau2, yty, bxxb, G, V, N, K)
+  
+  test_that("Weights are correct", {
+    expect_equal(Rout, Cout)
+  })
 })
