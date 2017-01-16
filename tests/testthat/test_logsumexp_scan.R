0237c2cbd79502cbb2dc7ae594241f15c0ca235c
emittman <emittman@iastate.edu>
2017-01-16 15:04:24 -0600

    Forgot to EXP

--- tests/testthat/test_logsumexp_scan.R
+++ tests/testthat/test_logsumexp_scan.R
@@ -5,7 +5,7 @@ dim <- 4e3
 x <- matrix(rnorm(dim^2), dim, dim)
 
 C_res <- matrix(.Call("Rnormalize_wts", x, as.integer(dim), as.integer(dim)), dim, dim)
-R_res <- log(apply(x, 2, cumsum))
+R_res <- log(apply(exp(x), 2, cumsum))
 
 test_that("Result is correct", {
   expect_equal(C_res, R_res)
