21259648a867876ffbe2c79171a125ab9a627a98
emittman <emittman@iastate.edu>
2017-01-18 11:12:26 -0600

    Use built in functional

--- src/host_fns/multinomial.cu
+++ src/host_fns/multinomial.cu
@@ -62,8 +62,7 @@ void gnl_multinomial(ivec_d &zeta, fvec_d &probs, curandState *states, int K, in
   ivec_d dummies(K*G);
   gRepEach<realIter>::iterator u_rep = getGRepEachIter(u.begin(), u.end(), K);
   compare_zip zipped = thrust::zip_iterator<compare_tup>(thrust::make_tuple(u_rep, probs.begin(), dummies.begin()));
-  is_greater f;
-  thrust::for_each(zipped, zipped + K*G, f);
+  thrust::for_each(zipped, zipped + K*G, thrust::greater<double>());
   
   repEachIter colI = getRepEachIter(K, 1);
   thrust::reduce_by_key(colI, colI + K*G, dummies.begin(), thrust::make_discard_iterator(), zeta.begin());
