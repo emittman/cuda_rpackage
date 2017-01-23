1e018c27d6ee34d2b938bacb1ccfaee070d4df6a
emittman <emittman@iastate.edu>
2017-01-23 14:49:52 -0600

    Dereference pointer

--- src/cudarpackage.cu
+++ src/cudarpackage.cu
@@ -199,9 +199,9 @@ extern "C" SEXP Rdata_wrap(SEXP Rdata){
   double *yty = REAL(VECTOR_ELT(Rdata, 0)),
          *xty = REAL(VECTOR_ELT(Rdata, 1)),
          *xtx = REAL(VECTOR_ELT(Rdata, 2));
-  int G = INTEGER(VECTOR_ELT(Rdata, 3)),
-      V = INTEGER(VECTOR_ELT(Rdata, 4)),
-      N = INTEGER(VECTOR_ELT(Rdata, 5));
+  int G = INTEGER(VECTOR_ELT(Rdata, 3))[0],
+      V = INTEGER(VECTOR_ELT(Rdata, 4))[0],
+      N = INTEGER(VECTOR_ELT(Rdata, 5))[0];
   data_t data(yty, xty, xtx, G, V, N);
   printVec(data.ytx, G, V);
 }
