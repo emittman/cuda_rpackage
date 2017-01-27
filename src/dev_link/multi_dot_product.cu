ccad8eab418d5b161fb849b51dc0490529c6af34
emittman <emittman@iastate.edu>
2017-01-27 10:15:39 -0600

    V -> dim

--- src/dev_link/multi_dot_product.cu
+++ src/dev_link/multi_dot_product.cu
@@ -22,8 +22,8 @@ void multi_dot_prod(fvec_d &x, fvec_d &y, fvec_d &z, int dim, int n){
   rowIter y_first = getRowIter(dim, 0);
   strideIter x = thrust::permutation_iterator<realIter, rowIter>(x.begin(), x_first);
   strideIter y = thrust::permutation_iterator<realIter, rowIter>(y.begin(), y_first);
-  dot_tuple my_tuple = thrust::make_tuple(x, y, z.begin());
+  dot_tup my_tuple = thrust::make_tuple(x, y, z.begin());
   thrust::zip_iterator<dot_tuple> zipped = thrust::zip_iterator<dot_tuple>(my_tuple);
-  dot_prod f(V);
+  dot_prod f(dim);
   thrust::for_each(zipped, zipped + n, f);
 }
