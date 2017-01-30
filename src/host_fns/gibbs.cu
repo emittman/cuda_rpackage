d442fdfb3f1896c2fe7ef340849336acd6fe9bb6
emittman <emittman@iastate.edu>
2017-01-30 16:40:27 -0600

    OK

--- src/host_fns/gibbs.cu
+++ src/host_fns/gibbs.cu
@@ -56,7 +56,7 @@ void draw_tau2(curandState *states, chain_t &chain, priors_t &priors, data_t &da
   //FltPermIter b_occ = thrust::permutation_iterator<realIter, intIter>(b.begin(), occ_begin);
   //thrust::transform(b_occ, b_occ + smry.num_occupied, sse.begin(), b_occ, modify_gamma_par_w_flt());
   std::cout << "b transformed:\n";
-  printVec(b_tmp, K, 1);
+  printVec(b_d, K, 1);
   // raw pointers
   //double *tau2_ptr = thrust::raw_pointer_cast(chain.tau2.data());
   //double *a_ptr = thrust::raw_pointer_cast(a.data());
