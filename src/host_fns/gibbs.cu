7dff9af45a84ca8a83ff856ea2d783ca8de6f5bb
emittman <emittman@iastate.edu>
2017-01-30 14:25:56 -0600

    Debug draw_tau2

--- src/host_fns/gibbs.cu
+++ src/host_fns/gibbs.cu
@@ -19,9 +19,12 @@ void draw_tau2(curandState *states, chain_t &chain, priors_t &priors, data_t &da
   typedef thrust::permutation_iterator<realIter, intIter> FltPermIter;
   IntPermIter Mk_iter =  thrust::permutation_iterator<intIter, intIter>(smry.Mk.begin(), smry.occupied.begin());
   thrust::transform(a.begin(), a.end(), Mk_iter, a.begin(), modify_gamma_par());
+  std::cout << "a transformed:\n";
+  printVec(a, priors.K, 1);
   FltPermIter sse_iter = thrust::permutation_iterator<realIter, intIter>(sse.begin(), smry.occupied.begin());
   thrust::transform(b.begin(), b.end(), sse_iter, b.begin(), modify_gamma_par());
-  
+  std::cout << "b transformed:\n";
+  printVec(b, priors.K, 1);
   // raw pointers
   double *tau2_ptr = thrust::raw_pointer_cast(chain.tau2.data());
   double *a_ptr = thrust::raw_pointer_cast(a.data());
