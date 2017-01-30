c128d5fc67c1e65c7c2ac45661cd40e8d60b68ee
emittman <emittman@iastate.edu>
2017-01-30 16:06:20 -0600

    Start at the beginning

--- src/host_fns/gibbs.cu
+++ src/host_fns/gibbs.cu
@@ -13,38 +13,40 @@ struct modify_gamma_par_w_flt: thrust::binary_function<double, double, double>{
 };
 
 void draw_tau2(curandState *states, chain_t &chain, priors_t &priors, data_t &data, summary2 &smry){
-  fvec_d sse(smry.num_occupied);
+  int K = chain.K;
   smry.sumSqErr(sse, chain.beta, data.xtx);
-  fvec_d a(priors.K, priors.a);
-  fvec_d b(priors.K, priors.b);
+  std::cout << "sse:\n";
+  printVec(sse, K, 1);
+  fvec_d a_tmp(K, priors.a);
+  fvec_d b_tmp(K, priors.b);
   std::cout << "a filled:\n";
-  printVec(a, priors.K, 1);
+  printVec(a_tmp, K, 1);
   // modify gamma parameters for occupied clusters
   //typedef thrust::permutation_iterator<intIter, intIter> IntPermIter;
-  typedef thrust::permutation_iterator<realIter, intIter> FltPermIter;
+  //typedef thrust::permutation_iterator<realIter, intIter> FltPermIter;
   //IntPermIter Mk_iter =  thrust::permutation_iterator<intIter, intIter>(smry.Mk.begin(), smry.occupied.begin());
-  std::cout << "I thought Mk was length " << smry.Mk.size() << "\n";
-  std::cout << "and that a was length " << a.size() << " \n";
-  printVec(smry.Mk, smry.K, 1);
+  //std::cout << "I thought Mk was length " << smry.Mk.size() << "\n";
+  //std::cout << "and that a was length " << a.size() << " \n";
+  //printVec(smry.Mk, K, 1);
   
-  intIter Mk_iter = smry.Mk.begin();
-  realIter a_begin = a.begin();
+  //intIter Mk_iter = smry.Mk.begin();
+  //realIter a_begin = a.begin();
   //modify_gamma_par_w_flt f;
   //thrust::transform(a_begin, a_begin + 1, Mk_iter, a_begin, f);
   
   std::cout << "a transformed:\n";
 
-  printVec(a, priors.K, 1);
+  printVec(a, K, 1);
   //intIter occ_begin = smry.occupied.begin();
   //FltPermIter b_occ = thrust::permutation_iterator<realIter, intIter>(b.begin(), occ_begin);
   //thrust::transform(b_occ, b_occ + smry.num_occupied, sse.begin(), b_occ, modify_gamma_par_w_flt());
   std::cout << "b transformed:\n";
-  printVec(b, priors.K, 1);
+  printVec(b, K, 1);
   // raw pointers
-  double *tau2_ptr = thrust::raw_pointer_cast(chain.tau2.data());
-  double *a_ptr = thrust::raw_pointer_cast(a.data());
-  double *b_ptr = thrust::raw_pointer_cast(b.data());
+  //double *tau2_ptr = thrust::raw_pointer_cast(chain.tau2.data());
+  //double *a_ptr = thrust::raw_pointer_cast(a.data());
+  //double *b_ptr = thrust::raw_pointer_cast(b.data());
   
   //generate
-  getGamma<<<chain.K, 1>>>(states, a_ptr, b_ptr, tau2_ptr);
+  //getGamma<<<K, 1>>>(states, a_ptr, b_ptr, tau2_ptr);
 }
