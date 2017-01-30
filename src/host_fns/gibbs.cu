0a4c8099e20a53701637ffa81bdf9f3904c2b596
emittman <emittman@iastate.edu>
2017-01-30 16:39:43 -0600

    OK

--- src/host_fns/gibbs.cu
+++ src/host_fns/gibbs.cu
@@ -14,8 +14,8 @@ struct modify_gamma_par_w_flt: thrust::binary_function<double, double, double>{
 
 struct modify_gamma_par {
   template<typename T>
-  void operator()(T tup){
-    thrust::get<0>(tup) = thrust::get<0>(tup) + 1.0;/// 2.0 * thrust::get<1>(tup);
+  __host__ __device__ void operator()(T tup){
+    thrust::get<0>(tup) = thrust::get<0>(tup) + 1.0/ 2.0 * thrust::get<1>(tup);
   }
 };
 
@@ -25,16 +25,16 @@ void draw_tau2(curandState *states, chain_t &chain, priors_t &priors, data_t &da
   smry.sumSqErr(sse, chain.beta, data.xtx);
   std::cout << "sse:\n";
   printVec(sse, K, 1);
-  fvec_d a_tmp(K, priors.a);
-  fvec_d b_tmp(K, priors.b);
-  std::cout << "a filled:\n";
-  printVec(a_tmp, K, 1);
+  fvec_d a_d(K, priors.a);
+  fvec_d b_d(K, priors.b);
+  std::cout << "a_d filled:\n";
+  printVec(a_d, K, 1);
   // modify gamma parameters for occupied clusters
   //typedef thrust::permutation_iterator<intIter, intIter> IntPermIter;
   //typedef thrust::permutation_iterator<realIter, intIter> FltPermIter;
   typedef thrust::tuple<realIter, intIter> tuple1;
   typedef thrust::zip_iterator<tuple1> zip1;
-  tuple1 tup1 = thrust::tuple<realIter, intIter>(a_tmp.begin(), smry.Mk.begin());
+  tuple1 tup1 = thrust::tuple<realIter, intIter>(a_d.begin(), smry.Mk.begin());
   zip1 zp1 = thrust::zip_iterator<tuple1>(tup1);
   
   modify_gamma_par f;
@@ -51,8 +51,7 @@ void draw_tau2(curandState *states, chain_t &chain, priors_t &priors, data_t &da
   //thrust::transform(a_begin, a_begin + 1, Mk_iter, a_begin, f);
   
   std::cout << "a transformed:\n";
-
-  printVec(a_tmp, K, 1);
+  printVec(a_d, K, 1);
   //intIter occ_begin = smry.occupied.begin();
   //FltPermIter b_occ = thrust::permutation_iterator<realIter, intIter>(b.begin(), occ_begin);
   //thrust::transform(b_occ, b_occ + smry.num_occupied, sse.begin(), b_occ, modify_gamma_par_w_flt());
