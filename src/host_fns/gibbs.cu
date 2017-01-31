02968711d3810bf4f4598850aacb37af8b14142e
emittman <emittman@iastate.edu>
2017-01-31 12:44:55 -0600

    Syntax

--- src/host_fns/gibbs.cu
+++ src/host_fns/gibbs.cu
@@ -81,8 +81,8 @@ void draw_pi(curandState *states, chain_t &chain, priors_t &priors, summary2 &su
                     thrust::raw_pointer_cast(Vk.data()));
   std::cout <<"Vk:\n";
   printVec(Vk, K, 1);
-  fvec_d Ck(K);
-  transform_exclusive_scan(Vk.begin(), Vk.end(), Ck.begin(), log_1m(), 0, thrust::plus<double>());
+  fvec_d Ck(K, 0.0);
+  transform_inclusive_scan(Vk.begin()+1, Vk.end(), Ck.begin()+1, log_1m(), thrust::plus<double>());
   std::cout << "Ck:\n";
   printVec(Ck, K, 1);
   transform(Vk.begin(), Vk.end(), Ck.begin(), chain.pi.begin(), exp_log_plus());
