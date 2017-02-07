5a1a24a8b9cb1208f4b824c24abbb1ee1c09c080
emittman <emittman@iastate.edu>
2017-02-07 15:05:51 -0600

    Address compiler error

--- src/header/running_mean.h
+++ src/header/running_mean.h
@@ -4,12 +4,12 @@ struct update_mean{
   int power;
   __host__ __device__ update_mean(int p): power(p){}
   template<typename Tup>
-  __host__ __device__ operator()(Tup t){
+  __host__ __device__ void operator()(Tup t){
     thrust::get<0>(t) = thrust::get<0>(t) + (pow(thrust::get<1>(t),power) - thrust::get<0>(t)) / thrust::get<2>(t);
   }
 };
 
-typedef thrust::tuple<realIter, realIter, thrust::constant_iterator<int>> update_tup;
+typedef thrust::tuple<realIter, realIter, thrust::constant_iterator<int> > update_tup;
 typedef thrust::zip_iterator<update_tup> update_zip;
 
 void update_running_means(fvec_d &means, fvec_d &new_obs, int length, int step, int power);
