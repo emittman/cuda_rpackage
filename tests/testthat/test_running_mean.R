3867399875da5769cd620b15e4ca3e51eefffdaa
emittman <emittman@iastate.edu>
2017-02-07 15:13:04 -0600

    OK

--- tests/testthat/test_running_mean.R
+++ tests/testthat/test_running_mean.R
@@ -1,9 +1,9 @@
 context("Running means")
 set.seed(2817)
-pows <- c(1,2,3)
+pows <- as.integer(c(1,2,3))
 
-len <- 100
-N <- 30
+len <- as.integer(100)
+N <- as.integer(30)
 
 X <- matrix(rnorm(len*N), len, N);
 
@@ -12,7 +12,7 @@ for(p in pows){
   Rout <- drop(apply(X, 1, function(row) mean(row^p)))
   Cout <- X[,1]^p
   for(i in 2:N){
-    Cout <- .Call("Rtest_running_mean", Cout, X[,i], p, i)
+    Cout <- .Call("Rtest_running_mean", Cout, X[,i], p, as.integer(i))
   }
   
   test_that(paste(c("Means match. (power = ", p, ")"), collapse=""), {
