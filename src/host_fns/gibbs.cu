f5a2622416659974aa83c51e929763f25f810712
emittman <emittman@iastate.edu>
2017-02-13 17:09:08 -0600

    Use device_ptr for device to device copy

--- src/host_fns/gibbs.cu
+++ src/host_fns/gibbs.cu
@@ -160,7 +160,8 @@ void draw_beta(curandState *states, data_t &data, chain_t &chain, priors_t &prio
   realIter prec_end = prec.end();
   chol_multiple(prec_begin, prec_end, data.V, smry.num_occupied);
   //get cluster locations
-  thrust::copy(smry.xty_sums.begin(), smry.xty_sums.end(), betahat.begin());
+  thrust::device_ptr<double> xty_ptr = &smry.xty_sums[0];
+  thrust::copy(xty_ptr, xty_ptr + smry.num_occupied*data.V, betahat.begin());
   beta_hat(prec, betahat, smry.num_occupied, data.V);
   draw_MVNormal(states, betahat, prec, chain.beta, priors, smry);
 }
