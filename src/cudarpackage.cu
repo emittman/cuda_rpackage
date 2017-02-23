a57b54db83f789dc04fe24db361347e73d6252fc
emittman <emittman@iastate.edu>
2017-02-23 12:02:43 -0600

    Add missing ;

--- src/cudarpackage.cu
+++ src/cudarpackage.cu
@@ -252,7 +252,7 @@ extern "C" SEXP Rrun_mcmc(SEXP Rdata, SEXP Rpriors, SEXP Rchain, SEXP Rn_iter, S
   chain_t chain = Rchain_wrap(Rchain);
   int n_iter = INTEGER(Rn_iter)[0], thin = INTEGER(Rthin)[0],
   G_save = length(Ridx_save), seed = INTEGER(Rseed)[0];
-  int n_save = n_iter/thin + (n_iter % thin == 0 ? 0 : 1)
+  int n_save = n_iter/thin + (n_iter % thin == 0 ? 0 : 1);
   samples_t samples(n_save, G_save, data.V, INTEGER(Ridx_save));
   
   int verbose = INTEGER(Rverbose)[0];
