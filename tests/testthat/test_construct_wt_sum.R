4487af55c8aa4074459f688336cd3fa08bee71ab
emittman <emittman@iastate.edu>
2017-02-28 12:38:15 -0600

    OK

--- tests/testthat/test_construct_wt_sum.R
+++ tests/testthat/test_construct_wt_sum.R
@@ -4,8 +4,9 @@ K <- as.integer(500)
 V <- as.integer(5)
 N <- as.integer(10)
 
-data <- formatData(y=matrix(rnorm(G*N), G, N),
-                   X = matrix(rnorm(N*V), N, V), tranform_y=identity)
+y <- matrix(rnorm(G*N), G, N)
+X <- matrix(rnorm(N*V), N, V)
+data <- formatData(y, X, transform_y=identity)
 
 beta <- matrix(rnorm(K*V),V,K)
 pi <- rbeta(K, 1, 9)
@@ -16,7 +17,7 @@ chain <- formatChain(beta, pi, tau2, zeta)
 
 priors <- formatPriors(K, rcauchy(V), rexp(V), 1, 1, 1)
 
-Cmean <- .Call("Rtest_wt_sum", priors, chain)
+Cmean <- .Call("Rtest_wt_sum", data, priors, chain)
 Rmean <- rep(priors$mu_0, K) * rep(priors$lambda2, K) / rep(chain$tau2,each=V)
 
 test_that("Correct answer", {
