302e2dd0cb3b3868f84dccad208e8d17a788cf2e
emittman <emittman@iastate.edu>
2017-03-03 14:20:53 -0600

    OK

--- src/cudarpackage.cu
+++ src/cudarpackage.cu
@@ -243,7 +243,7 @@ extern "C" SEXP Rrun_mcmc(SEXP Rdata, SEXP Rpriors, SEXP Rchain, SEXP Rn_iter, S
   data_t data = Rdata_wrap(Rdata);
   priors_t priors = Rpriors_wrap(Rpriors);
   chain_t chain = Rchain_wrap(Rchain);
-  int n_iter = INTEGER(Rn_iter)[0], thin = INTEGER(Rthin)[0],
+  int n_iter = INTEGER(Rn_iter)[0], thin = INTEGER(Rthin)[0];
   int n_save_P = INTEGER(Rn_save_P)[0];
   int G_save = length(Ridx_save), seed = INTEGER(Rseed)[0];
   int n_save_g = n_iter/thin + (n_iter % thin == 0 ? 0 : 1);
