acb9531e592d3f73cf4917814d1033314f060b1c
emittman <emittman@iastate.edu>
2017-03-03 14:56:51 -0600

    Fix typo

--- src/host_fns/wrap_R.cu
+++ src/host_fns/wrap_R.cu
@@ -67,7 +67,7 @@ SEXP Cchain_wrap(chain_t &chain){
   SEXP out_meansquares = PROTECT(allocVector(REALSXP, chain.meansquares.size()));
   thrust::copy(chain.probs.begin(), chain.probs.end(), REAL(out_probs));
   thrust::copy(chain.means.begin(), chain.means.end(), REAL(out_means));
-  thrust::copy(chain.meansquare.begin(), chain.meansquares.end(), REAL(out_meansquares));
+  thrust::copy(chain.meansquares.begin(), chain.meansquares.end(), REAL(out_meansquares));
   SET_VECTOR_ELT(samples_out, 0, out_probs);
   SET_VECTOR_ELT(samples_out, 1, out_means);
   SET_VECTOR_ELT(samples_out, 2, out_meansquares);
