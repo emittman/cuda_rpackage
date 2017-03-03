2a8b7f8365e8653bb8cb3183badc77808c108b4f
emittman <emittman@iastate.edu>
2017-03-03 14:50:05 -0600

    Fix parentheses

--- src/host_fns/chain.cu
+++ src/host_fns/chain.cu
@@ -14,7 +14,7 @@ data_t::data_t(double* _yty, double* _xty, double* _xtx, int _G, int _V, int _N)
 samples_t::samples_t(int _n_save_g, int _n_save_P, int _G_save, int _K, int _V, int *idx):
     n_save_g(_n_save_g), n_save_P(_n_save_P), step_g(0), step_P(0), G_save(_G_save), K(_K), V(_V),
     save_idx(idx, idx + _G_save), save_beta(_n_save_g*_G_save*_V), save_tau2(_n_save_g*_G_save),
-    save_P(_n_save_P*_K*(_V+2), save_max_id(_n_save_g), save_num_occupied(_n_save_g)){}
+    save_P(_n_save_P*_K*(_V+2)), save_max_id(_n_save_g), save_num_occupied(_n_save_g){}
 
 void samples_t::write_g_samples(chain_t &chain, summary2 &smry){
   if(step_g < n_save_g){
