71e290285aa40b0435e152d045613e8d5b27d426
emittman <emittman@iastate.edu>
2017-03-06 10:49:23 -0600

    Transpose beta in the output ( K*(V+2)*n_P_save array )

--- src/host_fns/chain.cu
+++ src/host_fns/chain.cu
@@ -51,8 +51,7 @@ void samples_t::write_P_samples(chain_t &chain){
     // copy clusters to save_P
     thrust::copy(chain.pi.begin(), chain.pi.end(),
                  save_P.begin() + iter_size * step_P);
-    thrust::copy(chain.beta.begin(), chain.beta.end(),
-                 save_P.begin() + iter_size * step_P + K);
+    transpose<realIter>(chain.beta.begin(), chain.beta.end(), chain.V, chain.K, save_P.begin() + itersize_ * step_P + K);
     thrust::copy(chain.tau2.begin(), chain.tau2.end(),
                  save_P.begin() + iter_size * step_P + K*(V+1));
     step_P = step_P + 1;
