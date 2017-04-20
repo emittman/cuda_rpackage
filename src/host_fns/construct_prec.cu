e467fc567d2c8f79ce23f0b0e3f4706cab327960
emittman <emittman@iastate.edu>
2017-04-19 21:41:31 -0500

    OK

--- src/host_fns/construct_prec.cu
+++ src/host_fns/construct_prec.cu
@@ -30,7 +30,7 @@ void construct_prec(fvec_d &prec, summary2 &smry, priors_t &priors, chain_t &cha
     std::cout << "\nSelect occupied columns iterator:\n";
     thrust::copy(colIter, colIter + smry.num_occupied*V*V, std::ostream_iterator<int>(std::cout, " "));
     std::cout << "\nxtx_sums via tmp iterator:\n";
-    thrust::copy(xtx_sums_b, xtx_sums_e, std::ostream_iterator<double>(std::cout, " "));
+    thrust::copy(smry.xtx_sums.begin(), smry.xtx_sums.end(), std::ostream_iterator<double>(std::cout, " "));
     std::cout << "\nxtx_sums mapped to clusters:\n";
     printVec(prec, V*V, priors.K);
   }
