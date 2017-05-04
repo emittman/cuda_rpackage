5b0736885d7ad10a6301c86f6181fd76893952d3
emittman <emittman@iastate.edu>
2017-05-04 11:19:59 -0500

    as.integer

--- tests/testthat/test_cluster_weights.R
+++ tests/testthat/test_cluster_weights.R
@@ -58,7 +58,7 @@ for(i in 1:times){
     })
   })
   
-  Cout <- .Call("Rcluster_weights", data, chain, priors, 1);
+  Cout <- .Call("Rcluster_weights", data, chain, priors, as.integer(1));
 
   test_that("Weights are correct, given bxxb and bxty", {
     expect_equal(as.numeric(Rout), Cout)
