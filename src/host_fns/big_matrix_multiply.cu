36a96ec4d2ff07564334d888c0aa1fbe0a218b0b
emittman <emittman@iastate.edu>
2017-05-19 23:20:36 -0500

    Investigate big_matrix_multiply

--- src/host_fns/big_matrix_multiply.cu
+++ src/host_fns/big_matrix_multiply.cu
@@ -9,12 +9,18 @@ void big_matrix_multiply(fvec_d &A, fvec_d &B, fvec_d &big_grid, int a1, int a2,
   int lda = a1, ldb = b1, ldc = a2;
   cublasHandle_t handle;
   cublasCreate(&handle);
+  cublasStatus_t status;
   double *A_ptr = thrust::raw_pointer_cast(&(A[0]));
   double *B_ptr = thrust::raw_pointer_cast(&(B[0]));
   double *grid_ptr = thrust::raw_pointer_cast(&(big_grid[0]));
-  cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, a2, b2, a1,
+  status = cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, a2, b2, a1,
               &alpha, A_ptr, lda,
               B_ptr, ldb,
               &beta, grid_ptr, ldc);
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    std::cout << "data download failed\n";
+    cublasDestroy(handle);
+    return EXIT_FAILURE;
+  })
   cublasDestroy(handle);
 }
