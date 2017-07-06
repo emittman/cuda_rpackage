ed8e96af7c0c02b7f8a64cdeeb5fcfcc1e9f65fb
emittman <emittman@iastate.edu>
2017-07-06 12:02:20 -0500

    Fix typo

--- src/header/quadform2.h
+++ src/header/quadform2.h
@@ -3,10 +3,10 @@
 
 #include "iterator.h"
 
-void quadform_multipleK(fvec_d &beta, fvec_d &xtx, fvec &result, int K, int V);
+void quadform_multipleK(fvec_d &beta, fvec_d &xtx, fvec_d &result, int K, int V);
 
-void quadform_multipleGK(fvec_d &beta, fvec_d &xtx, fvec &result, int G, int K, int V);
+void quadform_multipleGK(fvec_d &beta, fvec_d &xtx, fvec_d &result, int G, int K, int V);
 
-void quadform_multipleMatch(fvec_d &beta, fvec_d &xtx, fvec &result, int K, int V);
+void quadform_multipleMatch(fvec_d &beta, fvec_d &xtx, fvec_d &result, int K, int V);
 
 #endif
