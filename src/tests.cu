2869e1635ecd1ca8e96d2c51597ec8f99cb241e2
emittman <emittman@iastate.edu>
2017-07-10 15:54:52 -0500

    OK

--- src/tests.cu
+++ src/tests.cu
@@ -46,7 +46,7 @@ extern "C" SEXP Rcluster_weights(SEXP Rdata, SEXP Rchain, SEXP Rpriors, SEXP Rve
   chain_t chain = Rchain_wrap(Rchain, verbose);
   priors_t priors = Rpriors_wrap(Rpriors, verbose);
   fvec_d grid(data.G*priors.K);
-  cluster_weights(grid, data, chain, 0);
+  cluster_weights_no_voom(grid, data, chain, 0);
 
   fvec_h grid_h(data.G*priors.K);
   thrust::copy(grid.begin(), grid.end(), grid_h.begin());
