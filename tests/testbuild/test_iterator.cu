818f443bb8a86922033ddf1b3b18caee9830e8de
emittman <emittman@iastate.edu>
2017-07-10 10:54:35 -0500

    call correct function

--- tests/testbuild/test_iterator.cu
+++ tests/testbuild/test_iterator.cu
@@ -16,12 +16,12 @@ int main(){
   thrust::copy(vec2.begin(), vec2.end(), std::ostream_iterator<int>(std::cout, " "));
   
   std::cout <<"\n repEach(5, 2), vec";
-  gRepEach<intIter>::iterator eachit = getRepEachIter(vec.begin(), vec.end(), 5, 2);
+  gRepEach<intIter>::iterator eachit = getGRepEachIter(vec.begin(), vec.end(), 5, 2);
   thrust::copy(eachit, eachit + 20, out.begin());
   thrust::copy(out.begin(), out.end(), std::ostream_iterator<int>(std::cout, " "));
   
   std::cout <<"\n repEach(5, 2), vec2";
-  gRepEach<intIter>::iterator eachit = getRepEachIter(vec2.begin(), vec2.end(), 5, 2);
+  gRepEach<intIter>::iterator eachit = getGRepEachIter(vec2.begin(), vec2.end(), 5, 2);
   thrust::copy(eachit, eachit + 20, out.begin());
   thrust::copy(out.begin(), out.end(), std::ostream_iterator<int>(std::cout, " "));
 
