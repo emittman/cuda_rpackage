6e49b09afdb6ec167a7089780efa67f35e56506a
emittman <emittman@iastate.edu>
2017-07-10 10:57:21 -0500

    ok

--- tests/testbuild/test_iterator.cu
+++ tests/testbuild/test_iterator.cu
@@ -12,6 +12,7 @@ int main(){
   thrust::sequence(vec2.begin(), vec2.end(), 0, 1);
   std::cout << "initialized vec:\n";
   thrust::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, " "));
+  /*
   std::cout << "initialized vec2:\n";
   thrust::copy(vec2.begin(), vec2.end(), std::ostream_iterator<int>(std::cout, " "));
   
@@ -19,7 +20,7 @@ int main(){
   gRepEach<intIter>::iterator eachit = getGRepEachIter(vec.begin(), vec.end(), 5, 2);
   thrust::copy(eachit, eachit + 20, out.begin());
   thrust::copy(out.begin(), out.end(), std::ostream_iterator<int>(std::cout, " "));
-  /*
+  
   std::cout <<"\n repEach(5, 2), vec2";
   eachit = getGRepEachIter(vec2.begin(), vec2.end(), 5, 2);
   thrust::copy(eachit, eachit + 20, out.begin());
