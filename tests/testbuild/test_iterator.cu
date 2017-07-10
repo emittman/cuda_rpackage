418f9effa30d4bc22869166c7ca8c2fb94ea9f1c
emittman <emittman@iastate.edu>
2017-07-10 10:48:22 -0500

    ok

--- tests/testbuild/test_iterator.cu
+++ tests/testbuild/test_iterator.cu
@@ -11,19 +11,19 @@ int main(){
   thrust::sequence(vec.begin(), vec.end(), 0, 1);
   thrust::sequence(vec2.begin(), vec2.end(), 0, 1);
   std::cout << "initialized vec:\n";
-  thrust::copy(vec.begin(), vec.out(), std::ostream_iterator<int>(std::cout, " "));
+  thrust::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, " "));
   std::cout << "initialized vec2:\n";
-  thrust::copy(vec2.begin(), vec2.out(), std::ostream_iterator<int>(std::cout, " "));
+  thrust::copy(vec2.begin(), vec2.end(), std::ostream_iterator<int>(std::cout, " "));
   
   std::cout <<"\n repEach(5, 2), vec"
   gRepEach<ivec>::iterator eachit = getRepEachIter(vec.begin(), vec.end(), 5, 2);
   thrust::copy(eachit, eachit + 20, out.begin());
-  thrust::copy(out.begin(), out.out(), std::ostream_iterator<int>(std::cout, " "));
+  thrust::copy(out.begin(), out.end(), std::ostream_iterator<int>(std::cout, " "));
   
   std::cout <<"\n repEach(5, 2), vec2"
   gRepEach<ivec>::iterator eachit = getRepEachIter(vec2.begin(), vec2.end(), 5, 2);
   thrust::copy(eachit, eachit + 20, out.begin());
-  thrust::copy(out.begin(), out.out(), std::ostream_iterator<int>(std::cout, " "));
+  thrust::copy(out.begin(), out.end(), std::ostream_iterator<int>(std::cout, " "));
 
   return 0;
 }
