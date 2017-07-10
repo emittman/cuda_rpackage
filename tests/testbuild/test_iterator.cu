95a120a76dedf306f59425ef7de08446d8615c63
emittman <emittman@iastate.edu>
2017-07-10 11:58:14 -0500

    switch to double

--- tests/testbuild/test_iterator.cu
+++ tests/testbuild/test_iterator.cu
@@ -7,9 +7,9 @@
 
 int main(){
 
-  thrust::device_vector<int> vec(10);
-  thrust::device_vector<int> vec2(2);
-  thrust::device_vector<int> out(20);
+  fvec vec(10);
+  fvec vec2(2);
+  fvec out(20);
   
   thrust::sequence(vec.begin(), vec.end(), 0, 1);
   thrust::transform(vec.begin(), vec.end(), vec.begin(), thrust::placeholders::_1 + 0.1);
@@ -18,19 +18,19 @@ int main(){
 
   std::cout << "initialized vec:\n";
   
-  thrust::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, " "));
+  thrust::copy(vec.begin(), vec.end(), std::ostream_iterator<double>(std::cout, " "));
   
   std::cout << "initialized vec2:\n";
-  thrust::copy(vec2.begin(), vec2.end(), std::ostream_iterator<int>(std::cout, " "));
+  thrust::copy(vec2.begin(), vec2.end(), std::ostream_iterator<double>(std::cout, " "));
   
   std::cout <<"\n repEach(5, 2), vec\n";
-  gRepEach<intIter>::iterator eachit = getGRepEachIter(vec.begin(), vec.end(), 5, 2);
+  gRepEach<realIter>::iterator eachit = getGRepEachIter(vec.begin(), vec.end(), 5, 2);
   thrust::copy(eachit, eachit + 20, out.begin());
-  thrust::copy(out.begin(), out.end(), std::ostream_iterator<int>(std::cout, " "));
+  thrust::copy(out.begin(), out.end(), std::ostream_iterator<double>(std::cout, " "));
   std::cout <<"\n repEach(5, 2), vec2\n";
   eachit = getGRepEachIter(vec2.begin(), vec2.end(), 5, 2);
   thrust::copy(eachit, eachit + 20, out.begin());
-  thrust::copy(out.begin(), out.end(), std::ostream_iterator<int>(std::cout, " "));
+  thrust::copy(out.begin(), out.end(), std::ostream_iterator<double>(std::cout, " "));
   std::cout << "\n";
 
   return 0;
